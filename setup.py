061ff94f40e2f094ee1ab1caeab90f73a1666134
Matt McCormick <matt.mccormick@kitware.com>
2020-11-24 21:47:58 -0500

    ENH: Bump Python package to 1.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-polartransform',
-    version='1.1.1',
+    version='1.1.2',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
@@ -44,6 +44,6 @@ setup(
     keywords='ITK InsightToolkit',
     url=r'https://itk.org/',
     install_requires=[
-        r'itk-core>=5.1.1'
+        r'itk-core>=5.1.1.post1'
     ]
     )
